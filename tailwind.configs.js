000d5523d0317ec2f17925968dcd1530f132c5e9
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-02 22:27:59 +0200

    Adding Header

---
+++ tailwind.configs.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+        
+    "./pages/**/*.html",   
+    "./src/**/*.{js,ts,jsx,tsx}" // ✅ if you use React/Vue/etc
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
